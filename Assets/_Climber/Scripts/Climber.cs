a49a55d4d36c2d7d3fb2e0c08362d9d61a322cbd
Chad Ohl <chadohl@gmail.com>
2019-10-18 18:01:53 -0700

    Completed climbing system tutorial files

--- Assets/_Climber/Scripts/Climber.cs
+++ Assets/_Climber/Scripts/Climber.cs
@@ -5,4 +5,46 @@ using UnityEngine;
 public class Climber : MonoBehaviour
 {
 
+    public float gravity = 45.0f;
+    public float sensitivity = 45.0f;
+
+    private Hand currentHand = null;
+    private CharacterController controller = null;
+
+
+    private void Awake(){
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update(){
+        CalculateMovement();
+    }
+
+    private void CalculateMovement(){
+        Vector3 movement= Vector3.zero;
+
+        if (currentHand){
+            movement += currentHand.Delta * sensitivity;
+        }
+
+        if ( movement == Vector3.zero){
+            movement.y -= gravity * Time.deltaTime;
+        }
+        
+        controller.Move(movement * Time.deltaTime);
+    }
+
+    public void SetHand(Hand hand){
+        if (currentHand)
+        {
+            currentHand.ReleasePoint();
+        }
+
+        currentHand = hand;
+    }
+
+    public void ClearHand(){
+        currentHand = null;
+    }
 }
+
